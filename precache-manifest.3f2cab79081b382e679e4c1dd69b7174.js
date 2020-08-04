bf18e448c331ac5b27c0ff62a69a0d5c867d080b
unknown <lenovo@DESKTOP-DKDSO3N.utc.com>
2020-08-04 20:16:42 +0530

    Updates

---
+++ precache-manifest.3f2cab79081b382e679e4c1dd69b7174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deb0d3a9d8fc1aad1941eb5fcca1202",
+    "url": "/proj/index.html"
+  },
+  {
+    "revision": "4ac503e8fe453238769f",
+    "url": "/proj/static/css/main.3663952a.chunk.css"
+  },
+  {
+    "revision": "08447266c1f771fcb31c",
+    "url": "/proj/static/js/2.7a46a975.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proj/static/js/2.7a46a975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac503e8fe453238769f",
+    "url": "/proj/static/js/main.fe9ec05a.chunk.js"
+  },
+  {
+    "revision": "5930694895fcf3a4c15d",
+    "url": "/proj/static/js/runtime-main.10e616a7.js"
+  }
+]);
